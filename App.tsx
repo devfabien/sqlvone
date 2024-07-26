b4843f4570d2ba8b5bff67d9c40b7340954264ec
devfabien <ish001fabi@gmail.com>
2024-07-26 09:20:12 +0200

    feat: setup sqlite db

--- App.tsx
+++ App.tsx
@@ -1,20 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as SQLite from "expo-sqlite";
 
 export default function App() {
+  const [todo, setTodo] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [todoArray, setTodoArray] = useState<any>();
+
+  const db = SQLite.openDatabaseSync("todo.db");
+
+  useEffect(() => {
+    async function setup() {
+      await db.execAsync(
+        `CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, todo TEXT)`
+      );
+
+      const result = await db.getAllAsync(`SELECT * FROM todos`);
+      setTodoArray(result);
+    }
+    setup();
+  }, []);
+
+  const addTodo = async () => {
+    console.log(todo);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputStyles}
+          placeholder="Enter text"
+          onChangeText={setTodo}
+          value={todo}
+        />
+        <TouchableOpacity style={styles.button} onPress={addTodo}>
+          <Text>Add</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{ marginVertical: 10, padding: 10 }}>
+        {todoArray
+          ? todoArray.map((el: any) => {
+              return (
+                <View key={el.id} style={styles.dataContainer}>
+                  <Text style={styles.dataText}>{el.todo}</Text>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "orange",
+                      padding: 7,
+                      borderRadius: 8,
+                    }}
+                  >
+                    <Text>Del</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "aqua",
+                      padding: 7,
+                      borderRadius: 8,
+                    }}
+                  >
+                    <Text>Upd</Text>
+                  </TouchableOpacity>
+                </View>
+              );
+            })
+          : null}
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    padding: 10,
+  },
+  inputContainer: {
+    marginVertical: 19,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 7,
+    flexDirection: "row",
+    gap: 10,
+  },
+  inputStyles: {
+    width: "80%",
+  },
+  button: {
+    padding: 10,
+    borderRadius: 7,
+    backgroundColor: "yellow",
+  },
+  dataContainer: {
+    borderTopWidth: 1,
+    borderTopColor: "gray",
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 7,
+  },
+  dataText: {
+    width: "80%",
   },
 });
